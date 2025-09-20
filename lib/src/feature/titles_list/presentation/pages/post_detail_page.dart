ea577caa13d84d0163e203ab65413a5fbdb48f15
Murod <abdumajidov.murod@sarkor.uz>
2025-09-20 14:48:54 +0500

    UI part finished

--- lib/src/feature/titles_list/presentation/pages/post_detail_page.dart
+++ lib/src/feature/titles_list/presentation/pages/post_detail_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:user_app_test/src/feature/titles_list/domain/entity/post_entity.dart';
 
 class PostDetailPage extends StatelessWidget {
-  const PostDetailPage({super.key});
+  final PostEntity post;
+  const PostDetailPage({super.key, required this.post});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text(post.title)),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("ID: ${post.id}"),
+            const SizedBox(height: 8),
+            Text("User ID: ${post.userId}"),
+            const SizedBox(height: 8),
+            Text(post.body),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/src/feature/titles_list/presentation/pages/posts_list_page.dart
+++ lib/src/feature/titles_list/presentation/pages/posts_list_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_app_test/src/feature/titles_list/presentation/bloc/posts_bloc.dart';
+import 'package:user_app_test/src/feature/titles_list/presentation/pages/post_detail_page.dart';
 
 class PostsListPage extends StatefulWidget {
   const PostsListPage({super.key});
@@ -8,8 +11,75 @@ class PostsListPage extends StatefulWidget {
 }
 
 class _PostsListPageState extends State<PostsListPage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<PostsBloc>().add(LoadPosts());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List of posts"),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: TextField(
+              controller: _controller,
+              onChanged: (value) {
+                context.read<PostsBloc>().add(SearchPosts(value));
+              },
+              decoration: const InputDecoration(
+                hintText: "Search by title...",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: BlocBuilder<PostsBloc, PostsState>(
+        builder: (context, state) {
+          if (state is PostsLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is PostsLoaded) {
+            return RefreshIndicator(
+              child: ListView.builder(
+                itemCount: state.filteredPosts.length,
+                itemBuilder: (context, index) {
+                  final post = state.filteredPosts[index];
+                  return ListTile(
+                    title: Text(post.title, style: TextStyle(fontSize: 14)),
+                    subtitle: Text(
+                      post.body,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => PostDetailPage(post: post),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+              onRefresh: () async {
+                context.read<PostsBloc>().add(RefreshPosts());
+              },
+            );
+          } else if (state is PostsError) {
+            return Center(child: Text("Error: ${state.message}"));
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+    );
   }
 }
